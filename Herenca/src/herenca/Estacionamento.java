744d9b7d535afc140e1d176c908e8dd4e2c02478
joaopedrobyju <joaopedrosesi80@gmail.com>
2023-09-21 16:41:25 -0300

    Utilizando a herença do Java

--- Herenca/src/herenca/Estacionamento.java
+++ Herenca/src/herenca/Estacionamento.java
@@ -8,5 +8,13 @@ public class Estacionamento {
                 5, CambioEnum.MANUAL, CombustivelEnum.GASOLINA);
 
         System.out.println(gol);
+
+        Moto hornet = new Moto("Moto Hornet 600 C", 2014, "Preto", "Hornet",
+                "Honda", 40000, true, true,
+                CombustivelEnum.GASOLINA, PatidaEnum.ELETRICA);
+
+        System.out.println(hornet);
     }
+
+
 }
